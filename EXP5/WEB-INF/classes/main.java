0a4e72ae1513bd96a7c52362b0da4ebab5bfa98a
Aaliya5 <124867094+Aaliya5@users.noreply.github.com>
2023-03-24 05:04:49 +0530

    Update main.java

--- EXP5/WEB-INF/classes/main.java
+++ EXP5/WEB-INF/classes/main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class HelloWorld extends HttpServlet {
+public class main extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException
