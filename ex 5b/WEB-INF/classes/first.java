364032c310d26d56c01d9a77fbd4934ea951022f
Aaliya5 <124867094+Aaliya5@users.noreply.github.com>
2023-03-24 13:48:03 +0530

    Update first.java

--- ex 5b/WEB-INF/classes/first.java
+++ ex 5b/WEB-INF/classes/first.java
@@ -2,7 +2,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class name extends HttpServlet {
+public class first extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
@@ -15,7 +15,7 @@ public class name extends HttpServlet {
         out.println("<body>");
         out.println("<h1>Hello " + name + "</h1>");
         out.println("<p>You are from " + location + "</p><br>");
-        out.println("<form action='./both' method='get'>");
+        out.println("<form action='./second' method='get'>");
         out.println("<label for='name'>Name:</label>");
         out.println("<input type='hidden' id='name' name='name' value="+name+"><br>");
         out.println("<label for='location'>Location:</label>");
