f7501e2c69bbdf17e27a78aed39c13a161865992
Aaliya5 <124867094+Aaliya5@users.noreply.github.com>
2023-03-24 06:44:26 +0530

    Update HitCounter.java

--- Ex 5c/WEB-INF/classes/HitCounter.java
+++ Ex 5c/WEB-INF/classes/HitCounter.java
@@ -4,7 +4,7 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class PageHitCounter extends HttpServlet {
+public class HitCounter extends HttpServlet {
 
    private int hitCount; 
 
@@ -24,18 +24,12 @@ public class PageHitCounter extends HttpServlet {
       hitCount++; 
       PrintWriter out = response.getWriter();
       String title = "Total Number of Hits";
-      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
-      
-      out.println(docType +
-         "<html>\n" +
-            "<head><title>" + title + "</title></head>\n" +
-            "<body bgcolor = \"#f0f0f0\">\n" +
-               "<h1 align = \"center\">" + title + "</h1>\n" +
-               "<h2 align = \"center\">" + hitCount + "</h2>\n" +
-            "</body>
-         </html>"
-      );
-   }
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1><center>" + title + "</center></h1>");
+        out.println("<h1><center>" + hitCount + "</center></h1>");
+        out.println("</body>");
+        out.println("</html>");
    
    public void destroy() { 
       // This is optional step but if you like you
