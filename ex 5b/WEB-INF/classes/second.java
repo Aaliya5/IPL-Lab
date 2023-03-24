c1cee44be02f0b770815634adb229dad67ffeffe
Aaliya5 <124867094+Aaliya5@users.noreply.github.com>
2023-03-24 14:49:11 +0530

    Update second.java

--- ex 5b/WEB-INF/classes/second.java
+++ ex 5b/WEB-INF/classes/second.java
@@ -8,15 +8,10 @@ public class second extends HttpServlet {
         PrintWriter out = response.getWriter();
         String name = request.getParameter("name");
         String location = request.getParameter("location");
-        String age = request.getParameter("age");
         out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Hello " + name + "</title>");
-        out.println("</head>");
         out.println("<body>");
         out.println("<h1>Hello " + name + "</h1>");
-        out.println("<p>You are from " + location + "</p><br>");
-        out.println("<p>Your age is "+age+"</p>");
+        out.println("<p>Your location is " + location + "</p><br>");
         out.println("</body>");
         out.println("</html>");
     }
