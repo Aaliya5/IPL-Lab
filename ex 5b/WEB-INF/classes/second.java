3a3e5209174f084dd37aaf332a53cae9804e42db
Aaliya5 <124867094+Aaliya5@users.noreply.github.com>
2023-03-24 13:49:08 +0530

    Update second.java

--- ex 5b/WEB-INF/classes/second.java
+++ ex 5b/WEB-INF/classes/second.java
@@ -2,7 +2,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class both extends HttpServlet {
+public class second extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
